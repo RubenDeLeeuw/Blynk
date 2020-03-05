2d0c43d055aebfe3d983c66d2de84899a847c064
RubenDeLeeuw <61274077+RubenDeLeeuw@users.noreply.github.com>
2020-03-05 14:14:48 +0100

    Add files via upload

---
+++ Blynk_Control.ino
@@ -0,0 +1,42 @@
+#define BLYNK_PRINT Serial
+
+int pin        = 21;
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "MyHCTU1ii-Fbzf1KTZXzLPv3eqJ_Xxq2";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "IoTLabo";
+char pass[] = "broodrooster";
+
+void setup() {  
+  pinMode(pin, OUTPUT); 
+  pinMode(pin, HIGH);
+  Serial.begin(115200);
+
+  delay(10);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, pass);
+  int wifi_ctr = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("WiFi connected");  
+
+  Blynk.begin("MyHCTU1ii-Fbzf1KTZXzLPv3eqJ_Xxq2", ssid, pass);
+
+}
+
+void loop(){
+    Blynk.run();
+}
